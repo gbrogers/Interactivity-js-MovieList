c3a1bdeb041caa0d56c4f395626d846a19fd2830
Giselle Rogers <85512909+gbrogers@users.noreply.github.com>
2021-06-23 14:19:35 -0600

    commit changes
    lab with intermediate excercises

--- index.js
+++ index.js
@@ -21,17 +21,29 @@ function addMovie(event) {
 }
 
 function deleteMovie(event) {
+  console.log(event);
+  let deletedMovie = event.target.parentNode.firstChild.textContent;
   event.target.parentNode.remove("li");
-  message.textContent = "Movie deleted!";
+  message.textContent = `${deletedMovie} deleted!`;
+  revealMessage();
 }
 
 function crossOffMovie(event) {
   event.target.classList.toggle("checked");
+  let movieName = event.target.textContent;
   if (event.target.classList.contains("checked")) {
-    message.textContent = "Movie Watched!";
+    message.textContent = `${movieName} Watched!`;
   } else {
-    message.textContent = "Movie added back!";
+    message.textContent = `${movieName} added back!`;
   }
+  revealMessage();
+}
+
+function revealMessage() {
+  setTimeout(() => {
+    message.classList.add("hide");
+  }, 1000);
+  message.classList.remove("hide");
 }
 
 document.querySelector("form").addEventListener("submit", addMovie);
